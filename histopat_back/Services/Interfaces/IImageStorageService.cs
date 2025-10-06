f7bf146cf51d8a2daecde3db4b3591b2daec54c1
Danilo Borges de Oliveira <130411929+danilo-oliv@users.noreply.github.com>
2025-10-06 15:16:48 +0000

    feat: Image Service

---
+++ histopat_back/Services/Interfaces/IImageStorageService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace histopat_back.Services.Interfaces;
+
+public interface IImageStorageService
+{
+    public void salvarImagem();
+    public void baixar();
+}
---
+++ histopat_back/Services/Local/LocalStorageService.cs
@@ -0,0 +1,17 @@
+using System;
+using histopat_back.Services.Interfaces;
+
+namespace histopat_back.Services.Local;
+
+public class LocalStorageService : IImageStorageService
+{
+    public void baixar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void salvarImagem()
+    {
+        throw new NotImplementedException();
+    }
+}
